5c33f27af4c17ac154f061cf98a123c288f654f2
Lin Lin Aung <linlinaung@Lins-Mac-mini.local>
2025-07-26 01:12:18 +0630

    Version Update

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='smart-file-manager',
-    version='1.0.0',
-    py_modules=['smart_file_manager'],  # this is your .py file (without .py)
+    version='1.1.0',
+    py_modules=['main'],  # this is your .py file (without .py)
     install_requires=[
         'watchdog'
     ],
     entry_points={
         'console_scripts': [
-            'smartfm = smart_file_manager:main',  # CLI: smartfm → runs main() in smart_file_manager.py
+            'smartfm = main:main',  # CLI: smartfm → runs main() in main.py
         ],
     },
 )
